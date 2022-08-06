5261a12ad3be8329f92a2f7084e18d4291f2075d
hmrahim <h.m.rahimnet@gmail.com>
2022-08-06 23:09:23 +0600

    commited

--- src/Components/MessageUi.js
+++ src/Components/MessageUi.js
@@ -5,15 +5,7 @@ import NewConnections from './NewConnections';
 const MessageUi = () => {
     return (
         <div className='w-[350px] mx-auto h-[600px] overflow-y-auto my-5 bg-white  '>
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
+            
             <NewConnections/>
             <MessagesList/>
             
